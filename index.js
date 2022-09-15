b4491403b17448ff013add3b97291c205af6f302
ReeZey <johannes@seved.net>
2022-09-15 15:48:42 +0200

    IT WORKS!

--- index.js
+++ index.js
@@ -1,83 +1,42 @@
+const screenshot = require('screenshot-desktop');
+var Jimp = require('jimp');
+
 const Net = require('net');
 const server = new Net.Server();
 const port = 1337;
 
-var Jimp = require('jimp');
-
-var imageBuffer = new Uint16Array(256*192);
-
-Jimp.read('image.png', (err, img) => {
-    if (err) throw err;
-    //#define ARGB16(a,r,g,b) ( ((a) << 15) | (r)|((g)<<5)|((b)<<10))
-
-    for(var i = 0; i < img.bitmap.data.length; i += 4){
-        var r = img.bitmap.data[i];
-        var g = img.bitmap.data[i + 1];
-        var b = img.bitmap.data[i + 2];
-        var a = img.bitmap.data[i + 3] ? 1 : 0;
-
-        var color = (((a) << 15) | (r)|((g)<<5)|((b)<<10));
-
-        imageBuffer[i / 4] = color;
-    }
-});
 
 server.listen(port, () => {
-    console.log(`Server listening for connection requests on socket localhost:${port}`);
+    console.log(`server running port: ${port}`);
 });
 
 server.on('connection', (socket) => {
     console.log('A new connection has been established.');
-	
-    var data_8 = new Uint8Array(imageBuffer.buffer, imageBuffer.byteOffset, imageBuffer.byteLength);
-    //var segmented = chunk(data_8, 536);
 
-    
-    socket.write(data_8);
+    socket.on('data', async (chunk) => {
+        console.log(new Date(), chunk);
 
-    //writeOneMillionTimes(socket, segmented);
+        if(chunk[0] == 1){
 
-    socket.on('data', (chunk) => {
-        console.log(chunk);
-    });
+            var img = await screenshot();
+            var test = await Jimp.read(img);
+            var rescaled = test.resize(256, 192);
+        
+            var imageBuffer = new Uint16Array(256*192);
 
-    socket.on('end', () => {
-        console.log('Closing connection with the client');
-    });
-	
-    socket.on('error', (err) => {
-        console.log(err);
+            for(var i = 0; i < rescaled.bitmap.data.length; i += 4){
+                var r = rescaled.bitmap.data[i] / 8;
+                var g = rescaled.bitmap.data[i + 1] / 8;
+                var b = rescaled.bitmap.data[i + 2] / 8;
+                var a = rescaled.bitmap.data[i + 3] ? 1 : 0;
+        
+                var color = (((a) << 15) | (r)|((g)<<5)|((b)<<10));
+        
+                imageBuffer[i / 4] = color;
+            }
+
+            var data_8 = new Uint8Array(imageBuffer.buffer, imageBuffer.byteOffset, imageBuffer.byteLength);
+            socket.write(data_8);
+        }
     });
 });
-/*
-const timer = ms => new Promise(res => setTimeout(res, ms))
-
-function writeOneMillionTimes(socket, data) {
-    var i = 0;
-
-    let write = async () => {
-        var ok = true;
-
-        while(ok && i < data.length){
-            ok = socket.write(data[i]);
-
-            //await timer(10);
-
-            i += 1;
-        };
-
-        //writer.once('drain', () => setTimeout(write, 100));
-        socket.once('drain', () => write);
-    }
-
-    write();
-  }
-
-function chunk(s, maxBytes) {
-    const result = [];
-    for (var i = 0; i < s.length; i += maxBytes){
-        result.push(s.slice(i, i + maxBytes));
-    }
-    return result;
-}
-*/
