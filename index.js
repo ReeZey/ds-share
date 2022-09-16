81e2ecc0fc623e8fba8d85380aa62f5f228b29ab
ReeZey <johannes@seved.net>
2022-09-16 19:58:08 +0200

    more changes

--- index.js
+++ index.js
@@ -17,25 +17,26 @@ server.on('connection', (socket) => {
         console.log(new Date(), chunk);
 
         if(chunk[0] == 1){
-
+            console.time("convert");
+            console.time("screenshot");
             var img = await screenshot();
+            console.timeEnd("screenshot");
+            console.time("read");
             var test = await Jimp.read(img);
+            console.timeEnd("read");
+            console.time("resize");
             var rescaled = test.resize(256, 192);
+            console.timeEnd("resize");
         
+            console.time("color fix");
             var imageBuffer = new Uint16Array(256*192);
-
             for(var i = 0; i < rescaled.bitmap.data.length; i += 4){
-                var r = rescaled.bitmap.data[i] / 8;
-                var g = rescaled.bitmap.data[i + 1] / 8;
-                var b = rescaled.bitmap.data[i + 2] / 8;
-                var a = rescaled.bitmap.data[i + 3] ? 1 : 0;
-        
-                var color = (((a) << 15) | (r)|((g)<<5)|((b)<<10));
-        
-                imageBuffer[i / 4] = color;
+                imageBuffer[i >> 2] = (((rescaled.bitmap.data[i + 3] ? 1 : 0) << 15) | (rescaled.bitmap.data[i] >> 3) |((rescaled.bitmap.data[i + 1] >> 3)<<5)|((rescaled.bitmap.data[i + 2] >> 3)<<10));
             }
+            console.timeEnd("color fix");
 
             var data_8 = new Uint8Array(imageBuffer.buffer, imageBuffer.byteOffset, imageBuffer.byteLength);
+            console.timeEnd("convert");
             socket.write(data_8);
         }
     });
